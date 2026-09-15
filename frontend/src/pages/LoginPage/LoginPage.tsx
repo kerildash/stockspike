6812ef5206be3eb68bc0a13c641b01147fdbb977
kerildash <kerildash@gmail.com>
2026-09-15 20:41:19 +0300

    Fix button visual issues on LoginPage and RegisterPage

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -103,7 +103,7 @@ export const LoginPage: FC<ILoginPageProps> = () => {
               </div>
               <button
                 type="submit"
-                className="w-full text-gray-800 border-1 border-gray-400 cursor-pointer hover:border-gray-800 duration-200 focus:ring-1 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-gray-800 border-1 border-gray-400 cursor-pointer hover:border-gray-800 duration-200 focus:border-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Log in
               </button>
