f4e11384e2cd150feaef9f1ab83b2a24ee6da27d
kerildash <kerildash@gmail.com>
2026-09-15 22:49:50 +0300

    Fix icon margin in search bar

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -22,7 +22,7 @@ const Search: FC<SearchProps> = ({ onChange, onKeyDown, search, startSearch, sty
           className="w-full px-4 py-2 pl-10 text-gray-900 bg-white border-1 border-gray-200 rounded-lg hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-200 transition-colors duration-200"
         />
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <IoSearch className="mt-0.75 w-4 h-4 text-gray-400" />
+          <IoSearch className="w-4 h-4 text-gray-400" />
         </div>
       </div>
     </section>
