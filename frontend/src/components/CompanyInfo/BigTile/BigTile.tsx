a22a95ef3bb44af2f5b74964d1fbaf0871263389
kerildash <kerildash@gmail.com>
2026-09-16 00:55:55 +0300

    Fix big tile overflowing text issue

--- frontend/src/components/CompanyInfo/BigTile/BigTile.tsx
+++ frontend/src/components/CompanyInfo/BigTile/BigTile.tsx
@@ -7,11 +7,11 @@ interface IBigTileProps {
 
 export const BigTile: FC<IBigTileProps> = ({title, info}: IBigTileProps) => {
   return (
-          <div className='bg-white rounded-lg p-5 border border-gray-200'>
-            <h5 className='text-sm font-medium text-gray-800  mb-1'>
+          <div className='min-w-10 bg-white rounded-lg p-5 border border-gray-200'>
+            <h5 className='text-sm font-medium text-gray-800 mb-1'>
               {title}
             </h5>
-            <span className='text-4xl font-light'>{info}</span>
+            <span className='text-4xl font-light  break-words'>{info}</span>
           </div>
   );
 };
