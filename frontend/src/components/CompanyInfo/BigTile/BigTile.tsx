9d8063c12ae2b9ed99b49952cc7ea5f139b1164f
kerildash <kerildash@gmail.com>
2026-09-15 17:02:57 +0300

    Minor dashboard layout updates

--- frontend/src/components/CompanyInfo/BigTile/BigTile.tsx
+++ frontend/src/components/CompanyInfo/BigTile/BigTile.tsx
@@ -7,9 +7,8 @@ interface IBigTileProps {
 
 export const BigTile: FC<IBigTileProps> = ({title, info}: IBigTileProps) => {
   return (
-  
-          <div className='bg-white rounded-lg  p-5 border border-gray-200'>
-            <h5 className='text-blueGray-400 uppercase text-xs mb-1'>
+          <div className='bg-white rounded-lg p-5 border border-gray-200'>
+            <h5 className='text-sm font-medium text-gray-800  mb-1'>
               {title}
             </h5>
             <span className='text-4xl font-light'>{info}</span>
